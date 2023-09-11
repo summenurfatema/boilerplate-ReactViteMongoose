3cb7f52d004eb6538886fdc6ece0ecca78b98f19
jannatun-nur <shadrinmoni15@gmail.com>
2023-09-11 13:09:54 +0600

    private route setuped

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,9 +7,12 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import router from './Routes/Router';
+import UserContext from './Context/UserContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <UserContext>
     <RouterProvider router={router} />
+    </UserContext>
   </React.StrictMode>,
 )
