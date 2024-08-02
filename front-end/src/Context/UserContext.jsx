c74a6d45793d01f40b38c06e499912b1cd16f420
summenurfatema <shadrinmoni15@gmail.com>
2024-08-02 08:41:50 +0600

    redux implimentend

--- front-end/src/Context/UserContext.jsx
+++ front-end/src/Context/UserContext.jsx
@@ -1,23 +1,143 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import { useGetAllUsersQuery, useGetSingleUserQuery } from "../redux/features/auth/authApi";
+import { useSelector } from "react-redux";
 
 
-export const AuthContext = createContext()
-
+export const AuthContext = createContext();
 
 const UserContext = ({ children }) => {
+  const [loading, setLoading] = useState(true);
+  const [darkMode, setDarkMode] = useState(
+    localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+  );
 
-    const [loading, setLoading] = useState(true)
+  const { user } = useSelector((state) => state.auth);
+  const userId = user?._id;
+
+  //************************************************************************************************************** */
+  //********************************************     GET   ******************************************************* */
+  //************************************************************************************************************** */
+
+  //-------------- get each user info
+  const {
+    data: singleUser,
+    isLoading: isFetchingUser,
+    error: userError,
+  } = useGetSingleUserQuery(userId, { skip: !userId });
+
+  //-------------- get all user
+  const {
+    data: getAllUsers,
+    isLoading: isFetchingAllUsers,
+    error: allUserError,
+  } = useGetAllUsersQuery(userId, { skip: !userId });
 
 
-  //passable props
-  const authInfo = { loading,setLoading}
+  
 
-    return (
-        <AuthContext.Provider value={authInfo}>
-            {children}
-        </AuthContext.Provider>
 
-    );
+
+  //************************************************************************************************************** */
+  //********************************************     POST   ******************************************************* */
+  //************************************************************************************************************** */
+
+
+  
+  
+  //-------------- post skill of specific user
+//   const [
+//     addSkills,
+//     { data: responseAddSkillData, isFatchingAddSkill, error: addSkillError },
+//   ] = useAddSkillsMutation();
+
+  
+
+  //************************************************************************************************************** */
+  //********************************************     PUT   ******************************************************* */
+  //************************************************************************************************************** */
+
+  // ---------- update user skill
+//   const [
+//     updateSkills,
+//     { isUpdateSkillLoading, error: responseUpdateSkillError },
+//   ] = useUpdateSkillsMutation();
+
+  
+
+  //************************************************************************************************************** */
+  //********************************************     DELETE   ******************************************************* */
+  //************************************************************************************************************** */
+
+  //************************************************************************************************************** */
+  //********************************************    FETCH DATA   ************************************************* */
+  //************************************************************************************************************** */
+
+  useEffect(() => {
+    if (
+      isFetchingUser ||
+      isFetchingAllUsers       
+    ) {
+      setLoading(true);
+    } else {
+      setLoading(false);
+    }
+  }, [
+    isFetchingUser,
+    isFetchingAllUsers,
+
+  ]);
+
+  //************************************************************************************************************** */
+  //********************************************    FETCH ERROR   ************************************************* */
+  //************************************************************************************************************** */
+
+  useEffect(() => {
+    if (
+      userError ||
+      allUserError 
+
+      
+    ) {
+      console.error("Error fetching user data:", {
+        userError,
+        allUserError,
+        
+      });
+    }
+  }, [
+    userError,
+    allUserError,
+
+  ]);
+
+  //************************************************************************************************************** */
+  //********************************************    EXPORT DATA   ************************************************* */
+  //************************************************************************************************************** */
+
+  const shareableData = {
+    loading,
+    user,
+    getAllUsers,
+    setLoading,
+    darkMode,
+    setDarkMode,
+    singleUser,
+   
+  };
+
+  return (
+    <AuthContext.Provider value={shareableData}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+UserContext.propTypes = {
+  children: PropTypes.node.isRequired,
 };
 
 export default UserContext;
+
--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -1,18 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
+import { Provider } from "react-redux";
 import './index.css'
 import {
 
   RouterProvider,
 } from "react-router-dom";
+import { store } from "./redux/app/store.js";
 import router from './Routes/Router';
 import UserContext from './Context/UserContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Provider store={store}>
     <UserContext>
     <RouterProvider router={router} />
     </UserContext>
+    </Provider>
   </React.StrictMode>,
 )
